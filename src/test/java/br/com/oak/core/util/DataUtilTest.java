dc3706470beee93170b70392e97c38c06f8fe857
Gustavo Barcelos <gmbarcelos2@yahoo.com.br>
2015-06-29 23:39:21 -0300

    Adicionado classe de teste

---
+++ src/test/java/br/com/oak/core/util/DataUtilTest.java
@@ -0,0 +1,14 @@
+package br.com.oak.core.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
